1e6957962fcefa8dd83a8195b6d4eed78ae8d38f
nicoescudero <nicolas.escudero00@gmail.com>
2022-11-04 21:47:25 -0300

    dotenv setup

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
+const dotenv = require('dotenv');
 
 const indexRouter = require('./routes/index.routes');
 
 const app = express();
 
+dotenv.config();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
